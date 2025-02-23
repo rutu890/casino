da4d21dbea42462d35ab8f5e6353568d0b2440ba
Sibesh Patel <rutujakale890@gmail.com>
2025-02-23 22:22:03 +0530

    Initial commit

--- project.js
+++ project.js
@@ -70,8 +70,9 @@ const spin = () => {
     }
     //console.log(symbols);
 
-    const reels=[[],[],[]];
+    const reels=[];
     for (let i=0; i < COLS; i++) {
+        reels.push([]);
         const reelSymbols = [...symbols];
         for (let j = 0; j < ROWS; j++){
             const randomIndex = Math.floor(Math.random() * reelSymbols.length);
@@ -85,12 +86,43 @@ const spin = () => {
 
 };
 
-const reels = spin();
+const transpose = (reels) => {
+    const rows = [];
 
-console.log(reels);
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows;
+};
+
+const printRows= (rows) => {
+    for(const row of rows) {
+        //let rowString = " A | B | C ";
+        let rowString = "A";
+        for (const [i, symbol] of row.entries()) {
+            rowString += symbol
+            if (i != row.length -1 ) {
+                rowString += " | "
+            }
+            
+        }
+        console.log(rowString)
+    }
+}
+
 
 //const depositAmount=deposit();
 let balance=deposit();
 const numberOfLines=getNumberOfLines();
 const bet = getbet(balance,numberOfLines);
+const reels = spin();
+const rows=transpose(reels);
+
+
+
+
+
 
